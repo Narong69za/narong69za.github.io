53bc6c19ab8d26be7f5241d0c75d488bb1bf3d93
SP-Unlocked <164581482+Narong69za@users.noreply.github.com>
2026-02-11 16:26:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("SN DESIGN SERVER ONLINE 🔥");
+  res.send("🔥 SN DESIGN SERVER ONLINE 🔥");
 });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Server running on port " + PORT);
 });
