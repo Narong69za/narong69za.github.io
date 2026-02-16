aa98d9a6ac6d691dfea431502d6eef138bf8e804
SN DESIGN STUDIO <164581482+Narong69za@users.noreply.github.com>
2026-02-17 04:01:29 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,21 +7,9 @@ const app = express();
 
 app.use(express.json());
 
-/* ==============================
-   STATIC ROOT SERVE
-============================== */
-
-app.use(express.static(__dirname)); // serve ROOT html
-
-/* ==============================
-   API ROUTER
-============================== */
+app.use(express.static(__dirname)); // serve html root
 
 app.use('/api', require('./api/api.route'));
-
-/* ==============================
-   ROOT INDEX
-============================== */
 
 app.get('/', (req,res)=>{
    res.sendFile(path.join(__dirname,'index.html'));
