569b0c60b85efdabec3c6a3a06baf2d6569c8091
SN DESIGN STUDIO <164581482+Narong69za@users.noreply.github.com>
2026-02-17 06:16:24 +0700

    Update model.router.js

--- models/model.router.js
+++ models/model.router.js
@@ -1,1 +1,16 @@
+const MODEL_MAP={
 
+"dark-viral":"owner/model1",
+"ai-lipsync":"owner/model2",
+"dance-motion":"owner/model3",
+"face-swap":"owner/model4"
+
+};
+
+function getModel(template){
+
+return MODEL_MAP[template];
+
+}
+
+module.exports={ getModel };
