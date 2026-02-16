b01ab6782fa8ad47041abb32ad7c1924d2caf23e
SN DESIGN STUDIO <164581482+Narong69za@users.noreply.github.com>
2026-02-17 03:39:42 +0700

    Update ai.service.js

--- services/ai.service.js
+++ services/ai.service.js
@@ -1,1 +1,16 @@
+const Replicate = require("replicate");
 
+const replicate = new Replicate({
+   auth: process.env.REPLICATE_API_TOKEN
+});
+
+async function runAI(model,input){
+
+   const output = await replicate.run(model,{
+      input:input
+   });
+
+   return output;
+}
+
+module.exports = { runAI };
