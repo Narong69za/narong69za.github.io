b9c6cba697457ce091d4d8e25651d47c679c5b34
SN DESIGN STUDIO <164581482+Narong69za@users.noreply.github.com>
2026-02-18 05:00:45 +0700

    Update index.js

--- services/index.js
+++ services/index.js
@@ -10,19 +10,8 @@ const webhookRoute = require("../routes/webhook");
 const app = express();
 
 app.use(express.static(path.join(__dirname,"../public")));
+app.use("/assets", express.static(path.join(__dirname,"../assets")));
 
 app.get("/",(req,res)=>{
    res.sendFile(path.join(__dirname,"../public/index.html"));
-});
-
-app.use(express.json());
-
-app.use("/api/render", renderRoute);
-app.use("/api/status", statusRoute);
-app.use("/api/webhook", webhookRoute);
-
-const PORT = process.env.PORT || 10000;
-
-app.listen(PORT, () => {
-   console.log("ULTRA ENGINE LIVE ON:", PORT);
 });
