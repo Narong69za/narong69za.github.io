b4bc1647b87f06fd234d42af2d8b3e5b68a4c46b
SN DESIGN STUDIO <164581482+Narong69za@users.noreply.github.com>
2026-02-18 04:27:43 +0700

    Update index.js

--- services/index.js
+++ services/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-const path = require("path");
 
 require("../db/db");
 
@@ -9,24 +8,3 @@ const statusRoute = require("../routes/status");
 const webhookRoute = require("../routes/webhook");
 
 const app = express();
-
-app.use(express.json());
-
-app.use("/api/render",renderRoute);
-app.use("/api/status",statusRoute);
-app.use("/api/webhook",webhookRoute);
-
-app.use(express.json());
-
-app.use("/api/render",renderRoute);
-app.use("/api/status",statusRoute);
-
-app.use(express.static(path.join(__dirname,"../public")));
-
-app.get("/",(req,res)=>{
-   res.sendFile(path.join(__dirname,"../public","index.html"));
-});
-
-app.listen(process.env.PORT || 10000,()=>{
-   console.log("ULTRA ENGINE SQL LIVE");
-});
