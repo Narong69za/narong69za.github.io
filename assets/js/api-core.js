dc695a58d6347db8f3bcdbcd495e43bdf89bf30e
SN DESIGN STUDIO <164581482+Narong69za@users.noreply.github.com>
2026-02-17 14:08:19 +0700

    Update api-core.js

--- assets/js/api-core.js
+++ assets/js/api-core.js
@@ -1,42 +1,24 @@
-(function () {
+console.log("API CORE LOADED");
 
-const LOCAL_API = "http://localhost:10000";
-const PROD_API = "https://sn-design-api.onrender.com";
-
-const isLocal =
-location.hostname === "localhost" ||
-location.hostname === "127.0.0.1";
-
-window.API_BASE = isLocal ? LOCAL_API : PROD_API;
+window.API_BASE = "https://sn-design-api.onrender.com";
 
 window.apiFetch = async function(endpoint, options = {}) {
 
-try {
+   try{
 
-const url = window.API_BASE + endpoint;
+      const res = await fetch(window.API_BASE + endpoint,{
+         headers:{
+            "Content-Type":"application/json"
+         },
+         ...options
+      });
 
-console.log("API CALL:", url);
+      return await res.json();
 
-const res = await fetch(url, {
-headers: {
-"Content-Type": "application/json"
-},
-...options
-});
+   }catch(err){
 
-if(!res.ok){
-throw new Error("API ERROR: " + res.status);
-}
+      console.error("API ERROR:",err);
 
-return await res.json();
-
-}catch(err){
-
-console.error("API FAILED:", err);
-throw err;
+   }
 
 }
-
-};
-
-})();
