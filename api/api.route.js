da957acdf8ceb008cfb72eb905b152f9267ad800
SN DESIGN STUDIO <164581482+Narong69za@users.noreply.github.com>
2026-02-17 08:29:53 +0700

    Update api.route.js

--- api/api.route.js
+++ api/api.route.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 
-const auth = require('../services/auth.service');
-const credit = require("../services/credit.service")
+const auth = require("../services/auth.service");
+const credit = require("../services/credit.service");
 const usage = require("../usage-engine");
-const ai = require("../ai.service");
+const ai = require("../services/ai.service");
 
 
 /* ===================================
