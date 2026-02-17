3485042205ed47f6e6ebf67242204c8ee71925fe
SN DESIGN STUDIO <164581482+Narong69za@users.noreply.github.com>
2026-02-18 03:33:27 +0700

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,18 +1,23 @@
 const sqlite3 = require("sqlite3").verbose();
+const path = require("path");
 
-const db = new sqlite3.Database("./snstudio.db");
+const db = new sqlite3.Database(
+   path.join(__dirname,"ultra.db")
+);
 
 db.serialize(()=>{
 
    db.run(`
-      CREATE TABLE IF NOT EXISTS jobs (
+      CREATE TABLE IF NOT EXISTS projects(
          id TEXT PRIMARY KEY,
          templateID TEXT,
          engine TEXT,
          duration INTEGER,
          status TEXT,
          progress INTEGER,
-         createdAt INTEGER
+         creditUsed REAL,
+         eta TEXT,
+         createdAt DATETIME DEFAULT CURRENT_TIMESTAMP
       )
    `);
 
