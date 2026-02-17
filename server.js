2e1b9d808ff848bfd28caee02da7c07e8173292d
SN DESIGN STUDIO <164581482+Narong69za@users.noreply.github.com>
2026-02-18 02:12:02 +0700

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,19 @@ app.use(express.json());
 
 app.use("/api/render",renderRoute);
 app.use("/api/status",statusRoute);
+const path = require("path");
 
+/* ==========================================
+SERVE FRONTEND STATIC (ULTRA FIX)
+========================================== */
+
+app.use(express.static(path.join(__dirname,"public")));
+
+app.get("/",(req,res)=>{
+
+   res.sendFile(path.join(__dirname,"public","index.html"));
+
+});
 app.listen(process.env.PORT || 10000,()=>{
 
    console.log("ULTRA ENGINE LOCK ACTIVE");
