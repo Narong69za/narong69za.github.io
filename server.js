6de35d5b9136d35f517ee412fd293a53ef11d4cc
SN DESIGN STUDIO <164581482+Narong69za@users.noreply.github.com>
2026-02-18 02:46:53 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,50 +1,42 @@
-/* ============================
-ULTRA ENGINE FINAL SERVER
-============================ */
-
 const express = require("express");
 const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+
+/* ===================================
+LOAD DB
+=================================== */
+
+require("./db/db");
+
+/* ===================================
+API ROUTES
+=================================== */
 
 const renderRoute = require("./routes/render");
 const statusRoute = require("./routes/status");
 
-require("./db/db");
+app.use("/api/render", renderRoute);
+app.use("/api/status", statusRoute);
 
-const app = express();
-
-/* ============================
-MIDDLEWARE
-============================ */
-
-app.use(express.json());
-
-/* ============================
+/* ===================================
 STATIC FRONTEND
-============================ */
+=================================== */
 
 app.use(express.static(path.join(__dirname,"public")));
 
 app.get("/",(req,res)=>{
-
    res.sendFile(path.join(__dirname,"public","index.html"));
-
 });
 
-/* ============================
-API ROUTES
-============================ */
-
-app.use("/api/render",renderRoute);
-app.use("/api/status",statusRoute);
-
-/* ============================
+/* ===================================
 START SERVER
-============================ */
+=================================== */
 
 const PORT = process.env.PORT || 10000;
 
 app.listen(PORT,()=>{
-
-   console.log("ULTRA ENGINE LOCK ACTIVE ON PORT:",PORT);
-
+   console.log("ULTRA ENGINE LOCK ACTIVE ON:",PORT);
 });
