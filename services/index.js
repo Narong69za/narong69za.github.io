e7909144fcb241be5f558e9b227b71df2a083f6b
SN DESIGN STUDIO <164581482+Narong69za@users.noreply.github.com>
2026-02-21 06:55:27 +0700

    Update index.js

--- services/index.js
+++ services/index.js
@@ -1,17 +1,29 @@
 /*
 =====================================
-SERVICE INDEX FINAL
+REPLICATE SERVICE ENTRY
+SN DESIGN STUDIO
 =====================================
 */
 
-const modelRouter = require("./model.router");
+const faceClone = require("./replicate-face-clone");
+const imageGen = require("./replicate-image-gen");
 
-async function run(data){
+async function run(preset,data){
 
-   return await modelRouter.runModel(data);
+   console.log("REPLICATE RUN:", preset.id);
+
+   switch(preset.id){
+
+      case "replicate-face-clone":
+         return faceClone.run(data);
+
+      case "replicate-image-gen":
+         return imageGen.run(data);
+
+      default:
+         throw new Error("Replicate model not implemented: " + preset.id);
+   }
 
 }
 
-module.exports = {
-   run
-};
+module.exports = { run };
