d923e5d0378da2c3c856a94366be81183d9b02cd
SN DESIGN STUDIO <164581482+Narong69za@users.noreply.github.com>
2026-02-21 02:05:01 +0700

    Create runway-act-two.js

---
+++ presets/runway-act-two.js
@@ -0,0 +1,15 @@
+module.exports = {
+   id:"runway-act-two",
+   name:"Runway Act Two",
+   provider:"runwayml",
+   model:"act-two",
+   creditCost:25,
+   type:"video",
+   inputs:{
+      image:true,
+      motion:true
+   },
+   limits:{
+      maxDuration:10
+   }
+};
