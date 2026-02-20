85d4dba9369db48fc4afb53de2e516d69d059a13
SN DESIGN STUDIO <164581482+Narong69za@users.noreply.github.com>
2026-02-21 01:29:44 +0700

    Create lipsync.js

---
+++ presets/lipsync.js
@@ -0,0 +1,10 @@
+module.exports = {
+   id: "lipsync",
+   name: "Lip Sync",
+   provider: "replicate",
+   model: "owner/lipsync-model:version",
+   creditCost: 12,
+   limits: {
+      maxDuration: 20
+   }
+};
