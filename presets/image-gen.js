68a4b93be7544fc584ec448375bdbad08f051ac4
SN DESIGN STUDIO <164581482+Narong69za@users.noreply.github.com>
2026-02-21 16:27:37 +0700

    Update image-gen.js

--- presets/image-gen.js
+++ presets/image-gen.js
@@ -1,10 +1,7 @@
 module.exports = {
+
    id: "image-gen",
-   name: "Image Generator",
-   provider: "replicate",
-   model: "owner/model:version",
-   creditCost: 10,
-   limits: {
-      maxDuration: 10
-   }
+   creditCost: 5,
+   limits:{ maxDuration:0 }
+
 };
