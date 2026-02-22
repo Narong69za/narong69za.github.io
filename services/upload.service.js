b7ae64cc602f8fee3bbfab5809cf9cb903c8ac2e
SN DESIGN STUDIO <164581482+Narong69za@users.noreply.github.com>
2026-02-23 00:13:54 +0700

    Create upload.service.js

---
+++ services/upload.service.js
@@ -0,0 +1,23 @@
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+
+   destination:function(req,file,cb){
+
+      cb(null,"storage/input/");
+
+   },
+
+   filename:function(req,file,cb){
+
+      const name = Date.now()+"-"+file.originalname;
+      cb(null,name);
+
+   }
+
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;
