da14bb812ca3e65407da5ac0936523674ff8706c
SN DESIGN STUDIO <164581482+Narong69za@users.noreply.github.com>
2026-02-24 09:46:14 +0700

    Update runway.service.js

--- services/runway/runway.service.js
+++ services/runway/runway.service.js
@@ -1,40 +1,7 @@
-const fetch = require("node-fetch");
+const gen4 = require("./gen4video");
 
-const RUNWAY_API_KEY = process.env.RUNWAY_API_KEY;
+exports.run = async (data)=>{
 
-async function run({ model, prompt, jobID }){
-
-    const res = await fetch("https://api.runwayml.com/v1/generate",{
-
-        method:"POST",
-
-        headers:{
-            "Authorization":`Bearer ${RUNWAY_API_KEY}`,
-            "Content-Type":"application/json"
-        },
-
-        body:JSON.stringify({
-            model:model,
-            input:{
-                prompt:prompt
-            }
-        })
-
-    });
-
-    const data = await res.json();
-
-    return {
-
-        jobID,
-        output:data
-
-    };
-
-}
-
-module.exports = {
-
-    run
+   return gen4.run(data);
 
 };
