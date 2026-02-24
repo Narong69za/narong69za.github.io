025cf9a42027cc3ffd732056abf2928ebd3b3470
SN DESIGN STUDIO <164581482+Narong69za@users.noreply.github.com>
2026-02-24 09:42:54 +0700

    Update index.js

--- services/index.js
+++ services/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const multer = require("multer");
 
-const { create } = require("../controllers/create.controller.js");
+const { create } = require("../controllers/create.controller");
 
 const app = express();
 
