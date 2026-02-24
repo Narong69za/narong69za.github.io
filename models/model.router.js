c2ce0bc42960082e300cc1acb7bd447e87d4bb4a
SN DESIGN STUDIO <164581482+Narong69za@users.noreply.github.com>
2026-02-24 09:04:47 +0700

    Update model.router.js

--- models/model.router.js
+++ models/model.router.js
@@ -1,17 +1,18 @@
-const replicate = require("../services/replicate/replicate.service");
-const runway = require("../services/runway/runway.service");
+const replicate = require("../services/replicate/replicate.service.js");
+const runway = require("../services/runway/gen4video.js");
 
-exports.run = async (payload) => {
+exports.run = async (payload)=>{
 
-   const engine = payload.engine;
+   const {engine} = payload;
 
-   if (engine === "replicate") {
+   if(engine === "replicate"){
       return await replicate.run(payload);
    }
 
-   if (engine === "runway") {
+   if(engine === "runway"){
       return await runway.run(payload);
    }
 
    throw new Error("ENGINE NOT SUPPORTED");
+
 };
