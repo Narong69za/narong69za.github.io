f7c6ca47616aeaed3cddd5f878dbd0e839a81a2d
SN DESIGN STUDIO <164581482+Narong69za@users.noreply.github.com>
2026-02-26 05:43:20 +0700

    Update admin-api.js

--- admin/admin-api.js
+++ admin/admin-api.js
@@ -1,8 +1,38 @@
-async function adminFetch(url){
+const ADMIN_KEY = "sn_ultra_admin_2026";
+const API = "https://sn-design-api.onrender.com/admin-api";
 
-   const res=await fetch(url,{
-      headers:window.ADMIN_HEADERS
+async function loadUsers(){
+
+   const res = await fetch(`${API}/users`,{
+
+      headers:{
+         "x-admin-key": ADMIN_KEY
+      }
+
    });
 
-   return await res.json();
+   const data = await res.json();
+
+   console.log("USERS:",data);
+
+}
+
+async function addCredit(userId,amount){
+
+   await fetch(`${API}/add-credit`,{
+
+      method:"POST",
+      headers:{
+         "Content-Type":"application/json",
+         "x-admin-key": ADMIN_KEY
+      },
+      body: JSON.stringify({
+         userId,
+         amount
+      })
+
+   });
+
+   alert("เครดิตเพิ่มแล้ว");
+
 }
