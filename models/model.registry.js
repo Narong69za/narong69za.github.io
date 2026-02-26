867246d9a5b8c817048cf5d0916f07ec798e8a13
SN DESIGN STUDIO <164581482+Narong69za@users.noreply.github.com>
2026-02-26 20:02:04 +0700

    Create model.registry.js

---
+++ models/model.registry.js
@@ -0,0 +1,45 @@
+/*
+=================================================
+ULTRA MODEL REGISTRY
+ADD MODEL HERE ONLY
+=================================================
+*/
+
+module.exports = {
+
+   text_to_video: {
+
+      engine: "runwayml",
+      mode: "text_to_video"
+
+   },
+
+   image_to_video: {
+
+      engine: "runwayml",
+      mode: "image_to_video"
+
+   },
+
+   lip_sync: {
+
+      engine: "runwayml",
+      mode: "lipsync"
+
+   },
+
+   motion_transfer: {
+
+      engine: "runwayml",
+      mode: "motion"
+
+   },
+
+   face_swap: {
+
+      engine: "runwayml",
+      mode: "face"
+
+   }
+
+};
