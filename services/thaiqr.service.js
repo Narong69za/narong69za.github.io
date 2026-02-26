e026b79f7af0e7faf22537129e2501e05b046601
SN DESIGN STUDIO <164581482+Narong69za@users.noreply.github.com>
2026-02-26 18:31:04 +0700

    Update thaiqr.service.js

--- services/thaiqr.service.js
+++ services/thaiqr.service.js
@@ -1,23 +1,6 @@
 // =====================================================
-// SIMPLE PROMPTPAY QR (STATIC FORMAT)
+// THAI QR SERVICE (PROMPTPAY)
 // =====================================================
 
-const generatePayload = require("promptpay-qr");
-const QRCode = require("qrcode");
-
-// ต้อง npm install promptpay-qr qrcode
-
-exports.createQR = async ({ amount }) => {
-
-   const mobileNumber = process.env.PROMPTPAY_NUMBER; 
-   // ใส่เบอร์พร้อมเพย์คุณใน .env
-
-   const payload = generatePayload(mobileNumber, { amount });
-
-   const qrImage = await QRCode.toDataURL(payload);
-
-   return {
-      success:true,
-      qr: qrImage
-   };
-};
+const generatePayload = require("promptpay-qr-code");
+const QRCode = require("qrcode
