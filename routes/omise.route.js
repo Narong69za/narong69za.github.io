df27cc215ffe55bcedc38649c0270f31870a70fc
SN DESIGN STUDIO <164581482+Narong69za@users.noreply.github.com>
2026-03-02 03:48:56 +0700

    Update omise.route.js

--- routes/omise.route.js
+++ routes/omise.route.js
@@ -8,8 +8,8 @@
 
 const express = require("express");
 const Omise = require("omise");
+console.log("OMISE KEY:", process.env.OMISE_SECRET_KEY);
 const router = express.Router();
-
 const omise = Omise({
   publicKey: process.env.OMISE_PUBLIC_KEY,
   secretKey: process.env.OMISE_SECRET_KEY
